3a2763303a2515e846bb52db46a447b4c40051ef
RahmaaEsmail <rahmaaesmail26@gmail.com>
2025-12-07 11:16:20 +0200

    last-commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com' , 'encrypted-tbn0.gstatic.com'],
   },
+  experimental: {
+    turbo: false
+  }
 };
 
 export default nextConfig;
